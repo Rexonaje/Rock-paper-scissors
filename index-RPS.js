c8158b0d33fdda6856eb6e2fca7653778a78111e
Carlos Nicolas Brambilla <97968900+Rexonaje@users.noreply.github.com>
2022-07-25 16:36:18 -0300

    main code for the game
    this is the continuation of the Odins page exercise.
    here is the program made on JS and you use the google console to play, in the future i will be adding images and a UI to<<<<<<<<<<<< play on the screen of the page.

--- index-RPS.js
+++ index-RPS.js
@@ -1,1 +1,103 @@
-console.log("holi");
+
+
+function getRandomNumber(min,max){
+    min = Math.ceil(min);
+    max=  Math.floor(max);
+    return Math.floor(Math.random()* (max - min  + 1) + min);
+}
+ //console.log(getRandomNumber(1,3));
+function getRandoChoice(){
+    let choiceNumber= getRandomNumber(1,3);
+    let choice;
+    if(choiceNumber==1){
+        choice="rock";
+    }
+    else if(choiceNumber==2){
+        choice="papper";
+    }
+    else if(choiceNumber==3){
+        choice="scissors";
+    }
+return choice
+}
+//console.log(getRandoChoice());
+
+function PlayRound(playerSelection,ComputerSelection){
+    let winner;
+  
+    
+    playerSelection = prompt("choose your weapon!");
+    ComputerSelection = getRandoChoice();
+    
+    console.log("This Round:");
+    console.log("your chose : " + playerSelection);
+    console.log("PC chose : " + ComputerSelection);
+
+    playerSelection.toLowerCase();
+
+    function rockChose(){ 
+        if(ComputerSelection=="rock"){
+          winner="its a draw";
+        }
+        else if(ComputerSelection=="scissors"){
+            winner="you win!";
+        }
+        else if(ComputerSelection=="papper" ){
+            winner="you lose :(";
+        }
+        return winner
+    }
+    function scissorChose(){
+        if(ComputerSelection=="rock"){
+        winner="you lose :(";
+      }
+        else if(ComputerSelection=="scissors"){
+          winner="its a draw";
+      }
+        else if(ComputerSelection=="papper" ){
+          winner="you win";
+      }
+      return winner
+    }
+    function papperChose(){
+        if(ComputerSelection=="rock"){
+           winner="you win!" ;
+        }
+        else if(ComputerSelection=="scissors"){
+            winner="you lose";
+        }
+        else if(ComputerSelection=="papper"){
+            winner = "its a draw";
+        }    
+        return winner
+    }
+
+     
+    if(playerSelection=="rock"){
+    rockChose();
+    }
+    if(playerSelection=="scissors"){
+    scissorChose();
+    }
+    if(playerSelection=="papper"){
+    papperChose();
+    }
+   // if(playerSelection!= "rock" || playerSelection !="scissors"|| playerSelection !="papper"){
+     //   winner="chose a correct weapon!!";
+       // }
+return winner 
+}
+
+function PlayGame(score){
+    score=[];
+    
+    for (let i = 0; i<5 ;i++){
+     score[i]="round";   
+     score[i]= PlayRound();
+     
+      
+    }
+    return score
+}
+console.log( PlayGame());
+
